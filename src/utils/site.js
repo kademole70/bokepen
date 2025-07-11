5d91390d02a8e3a21fff33ce9459f263cd1def9a
kademole70 <kademole70@gmail.com>
2025-07-11 18:34:19 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://bokepen.pages.dev';
+export const nama = 'Bokepen';
+export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Colmek Smp Viral Sma Tobrut Tante';
+export const terbit = '2025-04-10T00:03:13Z';
+
+export const google = 'QAEqPvZTgLe_Jf4dHTxAngrCbITf8MfSXp4wGYuC0h0';
+export const yandex = '8e2b20ca035bdd11'
+
+export const index = '6aa489ed-af28-4515-8be9-fe45c59aced8';
